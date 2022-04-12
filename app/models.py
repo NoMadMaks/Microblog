b19a9f0bcf24739ccd6b5b9deb407aaa77dd284d
Maksim <maksim.borkovic@gmail.com>
2022-04-12 18:33:53 +0200

    Popups+50%working notifications

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
-from app import db, login
 from datetime import datetime
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from time import time
 from app import app
 import jwt
+import json
 
 followers = db.Table('followers', 
         db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -27,6 +28,7 @@ class User(UserMixin, db.Model):
     messages_sent = db.relationship('Message', foreign_keys='Message.sender_id', backref='author', lazy='dynamic')
     messages_received =  db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     def _repr_(self):
         return '<User {}>'.format(self.username)
@@ -58,17 +60,28 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'], algorithm='HS256')
 
-    def new_messages(self):
-        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
-    
     @staticmethod
     def verify_reset_password(token):
         try:
             id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except: 
             return
-        return User.query.get(id)    
+        return User.query.get(id)  
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+      
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -81,15 +94,23 @@ class Post(db.Model):
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
-    recipient_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index = True, default=datetime.now)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Message {}>'.format(self.body) 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
+
+
 
