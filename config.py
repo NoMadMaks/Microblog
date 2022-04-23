28494a4ba8a67b83c9a63bb0eaa9c15482ef36d7
Maksim <maksim.borkovic@gmail.com>
2022-04-23 20:53:48 +0200

    minor changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET KEY') or 'microblogtest'
+    SECRET_KEY = os.environ.get('SECRET KEY') or 'RoBlogtest'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -11,8 +11,8 @@ class Config(object):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = '587'
     MAIL_USE_TLS = 1
-    MAIL_USERNAME = 'enter email'
-    MAIL_PASSWORD = 'enter pass'
-    ADMINS = ['enter admin']
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') #http://localhost:9200
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'enter email'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'enter pass'
+    ADMINS = os.environ.get('ADMINS') or 'enter admin'
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     
--- tests.py
+++ tests.py
@@ -17,13 +17,7 @@ class UserModelCase(unittest.TestCase):
         u.set_password('cat')
         self.assertFalse(u.check_password('dog'))
         self.assertTrue(u.check_password('cat'))
-
-    def test_avatar(self):
-        u = User(username='john', email='john@example.com')
-        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
-                                         'd4c74594d841139328695756648b6bd6'
-                                         '?d=identicon&s=128'))
-
+        
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='susan', email='susan@example.com')
